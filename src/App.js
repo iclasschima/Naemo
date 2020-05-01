4b58fdcf251f810196de40b89ceb515adba625eb
iClass <iclasschima@gmail.com>
2020-05-02 00:35:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import Banner from "./components/Banner"
+import Navbar from "./components/Navbar"
+import Afriskaut from "./components/Afriskaut"
+import Footer from "./components/Footer"
+import Contact from "./components/Contact";
+import MobileNav from "./components/MobileNav"
+import About from "./components/About"
+import Content from "./components/Homecontent"
+import Error from "./components/Error"
+
+import {ThemeProvider} from "styled-components"
 
 function App() {
+
+  const [state, setState] = useState(false)
+
+  const changeState = () => {
+      setState(state => !state)
+  }
+
+  const theme = {
+    color: {
+      primary: "#E6702C",
+      secondary: "#1B203C"
+    },
+    fontSize: {
+      bg: "1.3rem",
+      md: "1rem",
+      sm: "0.7rem",
+      header: "26px"
+    },
+    device: {
+      ipadPro: "1024px",
+      iPad: "768px",
+      mobile: "480px"
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     
+      <ThemeProvider theme={theme}>
+      {state ? <MobileNav changeState={changeState}/> : ""}
+        <Banner />
+        <Navbar changeState={changeState}/>
+        <div style={{padding: "5% 0 10%"}}>          
+          <Switch>
+              <Route path="/" component={Content} exact/>
+              <Route path="/about" component={About} exact/>
+              <Route path="/contact" component={Contact} exact/>
+              <Route path="/afriskaut" component={Afriskaut} exact/>
+              <Route component={Error} />
+          </Switch>
+              
+        </div>
+        <Footer />
+      </ThemeProvider>
+
+    </BrowserRouter>
   );
 }
 
